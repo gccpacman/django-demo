2cf0f494ef40ab80f611a2199c4059f2a406eb8c
realhu1989 <huyunyan@live.cn>
2015-05-03 16:43:46 +0800

    part 4

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from .models import Question, Choice
 
@@ -7,16 +7,23 @@ from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('index.html')
+   # template = loader.get_template('index.html')
     context = RequestContext(request, {
         'latest_question_list':latest_question_list,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'index.html', context) 
+  #  return HttpResponse(template.render(context))
     #output = ', '.join([str((p.question_text, p.pub_date)) for p in latest_question_list])
     #return HttpResponse(output)
 
 def detail(request, poll_id):
-    return HttpResponse("You are looking at poll %s." % poll_id)
+    # try:
+    #   question = Question.objects.get(pk=poll_id)
+    # except Question.DoesNotExist:
+    #   raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=poll_id)
+    return render(request, 'detail.html', {'question': question})
+    # return HttpResponse("You are looking at poll %s." % poll_id)
 
 def results(request, poll_id):
     return HttpResponse('You are looking at the result of poll %s' % poll_id)
