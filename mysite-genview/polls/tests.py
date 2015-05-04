5690415e9fbb22ffcee68bb4668ecf05a103d247
realhu1989 <huyunyan@live.cn>
2015-05-04 15:46:22 +0800

    tutorial part5 test 3

--- mysite-genview/polls/tests.py
+++ mysite-genview/polls/tests.py
@@ -31,6 +31,32 @@ class QuestionMethodTest(TestCase):
         #self.assertQuerysetEqual(response.context['latest_question_list'], [''])
         self.assertContains(response, "No polls are available", status_code=200)
     
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        should be displayed.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_index_view_with_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )        
+    
+    ####    
     
     def test_was_published_recently_with_future_question(self):
         """
