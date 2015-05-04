3fedae08ef1815d5d0b51989edd82de35f4a500a
realhu1989 <huyunyan@live.cn>
2015-05-04 15:27:41 +0800

    part 5 test 2

--- mysite-genview/polls/tests.py
+++ mysite-genview/polls/tests.py
@@ -7,9 +7,30 @@ from django.core.urlresolvers import reverse
 from .models import Question
 
 
+# Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionMethodTest(TestCase):
+    
+    def test_index_view_with_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        
+    def test_index_view_with_a_past_question(self):    
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+        
+    def test_index_view_with_a_future_question(self):
+        create_question(question_text='Future Quesion', days=30)
+        response = self.client.get(reverse('polls:index'))
+        #self.assertQuerysetEqual(response.context['latest_question_list'], [''])
+        self.assertContains(response, "No polls are available", status_code=200)
+    
     
     def test_was_published_recently_with_future_question(self):
         """
--- mysite-genview/polls/views.py
+++ mysite-genview/polls/views.py
@@ -22,7 +22,7 @@ class IndexView(generic.ListView):
         """return the last five published questions"""
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        )order_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:5]
     
     
 class DetailView(generic.DetailView):
