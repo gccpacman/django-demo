e37600c56a11e06a67d4782571d5a942877e6623
realhu1989 <huyunyan@live.cn>
2015-06-10 09:20:36 +0800

    use template

--- djangogirls/VENV/mysite/trips/views.py
+++ djangogirls/VENV/mysite/trips/views.py
@@ -3,16 +3,18 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def hello_world(request):
-    output = """
-        <!DOCTYPE html>
-        <html>
-            <head>
-            </head>
-            <body>
-                Hello World! <em style="color:LightSeaGreen,">{current_time}</em>
-            </body>
-        </html>
-""".format(current_time=datetime.now())
-    return HttpResponse(output)
+#    output = """
+#        <!DOCTYPE html>
+#        <html>
+#            <head>
+#            </head>
+#            <body>
+#                Hello World! <em style="color:LightSeaGreen,">{current_time}</em>
+#            </body>
+#        </html>
+#""".format(current_time=datetime.now())
+# vim comment multiply lines --- ctrl+v choose line, <I>, <some content like:#>,
+# <ESC>
 
-
+#    return HttpResponse(output)
+    return render(request, "hello_world.html", {'current_time':datetime.now()})
