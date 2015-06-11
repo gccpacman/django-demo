df717d462824821af51af7b240093b237736c037
realhu1989 <huyunyan@live.cn>
2015-06-11 21:08:16 +0800

    init myprj proj

---
+++ myprj/polls/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Poll'
+        db.create_table(u'polls_poll', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal(u'polls', ['Poll'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Poll'
+        db.delete_table(u'polls_poll')
+
+
+    models = {
+        u'polls.poll': {
+            'Meta': {'object_name': 'Poll'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['polls']
