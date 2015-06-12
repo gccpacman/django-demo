0301e7b57975532135bd37dd18c91bb58979d378
realhu1989 <huyunyan@live.cn>
2015-06-12 20:51:58 +0800

    djangogirl template exec 1

--- djangogirls/VENV/mysite/mysite/urls.py
+++ djangogirls/VENV/mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from trips.views import hello_world
+from trips.views import home
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'hello', hello_world),
+    url(r'^$', home),
 )
--- djangogirls/VENV/mysite/trips/views.py
+++ djangogirls/VENV/mysite/trips/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from trips.models import Post
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {'post_list':post_list})
 
 def hello_world(request):
 #    output = """
